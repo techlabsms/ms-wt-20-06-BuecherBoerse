52a95042ed99fcedf5db8f1691ddf1cddecdbdd5
jaycas22 <74538728+jaycas22@users.noreply.github.com>
2021-01-27 11:56:40 +0100

    Add files via upload

---
+++ book.model.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+
+const BookSchema = new mongoose.Schema ({
+	name: {
+		type: String,
+		trim: true,
+		required: 'Name des Buches fehlt'		
+	},
+	author: {	
+		type: String,
+		trim: true,
+		required: 'Autor des Buches fehlt'		
+	},
+	category: {				
+		type: String,
+		trim: true,
+	},
+	language: {
+		type: String,
+		trim: true,
+	},
+	state: {		//Zustand des Buches
+		type: String,
+		trim: true,
+	},
+	isbn: {
+		type: Number,
+		trim: true,
+	},
+	//User ID, kann momentan nur manuell eingetragen werden 
+	owner: {			
+		type: String,
+		required: 'Besitzer des Buches fehlt'		
+	},
+	status: {	//privat, bereit zum Verleihen, verliehen	
+		type: String,
+		required: 'Status des Buches fehlt',	
+	},
+	created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: Date
+})
+
+export default mongoose.model('Book', BookSchema)
