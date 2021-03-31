1a50e624048ae0cbf21990077f4010c4fa5eb11c
Chris Schimetschka <chris.schimetschka@gmail.com>
2021-03-31 21:16:53 +0200

    prep deployment 5

--- server/express.js
+++ server/express.js
@@ -28,6 +28,10 @@ app.use('/', userRoutes);
 app.use('/', authRoutes);
 app.use('/', bookRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(CURRENT_WORKING_DIR, 'client/build/index.html'));
+});
+
 app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
     res.status(401).json({ error: err.name + ': ' + err.message });
