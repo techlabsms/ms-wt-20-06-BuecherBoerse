fd0ca8f86370db3cf9d690c484509e4371f3b353
Chris Schimetschka <chris.schimetschka@gmail.com>
2021-03-31 21:05:44 +0200

    prep deploy 3

--- server/express.js
+++ server/express.js
@@ -21,7 +21,7 @@ app.use(helmet());
 // Cross Origin Resource Sharing
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(CURRENT_WORKING_DIR, 'client/build')));
 
 // mount routes
 app.use('/', userRoutes);
