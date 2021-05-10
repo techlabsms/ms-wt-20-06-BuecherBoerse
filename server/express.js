1ecbfb1daacfbd0f0ab04fb7cbaf65ced7f81183
Chris Schimetschka <chris.schimetschka@gmail.com>
2021-05-11 01:00:56 +0200

    bring cors back

--- server/express.js
+++ server/express.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compress from 'compression';
-// import cors from 'cors'
+import cors from 'cors';
 import helmet from 'helmet';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
@@ -20,7 +20,7 @@ app.use(compress());
 // Secure apps
 app.use(helmet());
 // Cross Origin Resource Sharing
-// app.use(cors());
+app.use(cors());
 
 app.use(express.static(path.join(CURRENT_WORKING_DIR, 'client/build')));
 
